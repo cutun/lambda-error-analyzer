ed4e31fd53052dad46f33cf8c6be2572edc32ee1
SUPERIC520 <eric20050708@gmail.com>
2025-06-17 17:53:58 -0700

    test

--- cli/push_log.py
+++ cli/push_log.py
@@ -4,7 +4,7 @@ import json
 import uuid
 from datetime import datetime, timezone
 
-# No API Gateway yet.
+# No API Gateway yet
 API_ENDPOINT = "https://YOUR_API_GATEWAY_ENDPOINT_HERE/prod/logs"
 
 def create_log_payload(log_level, message, details=None):
